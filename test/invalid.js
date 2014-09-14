37a99de022ff9a519198343a72dbf1abe039ea6a
James Halliday <mail@substack.net>
2014-09-14 20:03:45 +0300

    invalid case should error

---
+++ test/invalid.js
@@ -0,0 +1,15 @@
+var hybrid = require('../');
+var test = require('tape');
+var fs = require('fs');
+var concat = require('concat-stream');
+
+var privkey = fs.readFileSync(__dirname + '/files/private');
+var pubkey = fs.readFileSync(__dirname + '/files/public');
+
+test('invalid', function (t) {
+    t.plan(2);
+    
+    var dec = hybrid.decrypt(privkey, { encoding: 'hex' });
+    dec.on('error', function (err) { t.ok(err) });
+    dec.end('whatever');
+});
